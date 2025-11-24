634adea74fac1c32fa06434af357d28e87238926
mayanksinghr <your-email@example.com>
2025-11-24 16:23:21 +0530

    corrected user creation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config/mongodb.js"
+import clearkWebhooks from "./routes/clearkWebhooks.js"
+
 
 
 
